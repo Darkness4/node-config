85320361e2fbb479b07c284157da96a9ca5337f5
Nguyen Marc <nguyen_marc@live.fr>
2024-02-23 19:46:53 +0100

    fix: respect shellcheck

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 chmod +x ./post.sh
-./post.sh | tee /var/log/gitops.log
-if [ $? -eq 0 ]; then
+if ./post.sh | tee /var/log/gitops.log; then
   curl -X POST -H "Content-Type: application/json" -F "file1=/var/log/gitops.log" -d '{"content":"GitOps: Success"}' https://en08lc5cdlo763.x.pipedream.net
 else
   curl -X POST -H "Content-Type: application/json" -F "file1=/var/log/gitops.log" -d '{"content":"GitOps: Failure"}' https://en08lc5cdlo763.x.pipedream.net
