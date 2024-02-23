ac68a14f208ad564ce2122098a6045520aa65c56
Nguyen Marc <nguyen_marc@live.fr>
2024-02-23 19:48:24 +0100

    fix: fix curl include

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 chmod +x ./post.sh
 if ./post.sh | tee /var/log/gitops.log; then
-  curl -X POST -H "Content-Type: application/json" -F "file1=/var/log/gitops.log" -F 'payload_json={"content":"GitOps: Success"}' https://en08lc5cdlo763.x.pipedream.net
+  curl -X POST -H "Content-Type: application/json" -F "file1=@/var/log/gitops.log" -F 'payload_json={"content":"GitOps: Success"}' https://en08lc5cdlo763.x.pipedream.net
 else
-  curl -X POST -H "Content-Type: application/json" -F "file1=/var/log/gitops.log" -F 'payload_json={"content":"GitOps: Failure"}' https://en08lc5cdlo763.x.pipedream.net
+  curl -X POST -H "Content-Type: application/json" -F "file1=@/var/log/gitops.log" -F 'payload_json={"content":"GitOps: Failure"}' https://en08lc5cdlo763.x.pipedream.net
 fi
